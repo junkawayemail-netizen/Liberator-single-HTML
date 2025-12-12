08ae7e1b01f12db33a45b8518733aa1c71952926
daniam <junkaway.email@gmail.com>
2025-12-12 10:05:45 -0800

    Update config-remove.js

--- config-remove.js
+++ config-remove.js
@@ -2,6 +2,6 @@
 // You can set your API key here to work without rebuilding the project.
 // Replace "YOUR_API_KEY_HERE" with your actual Google Gemini API Key.
 
-window.GEMINI_API_KEY = "AIzaSyCkSM-bmt0sy7-qLQFWbvuo3hzFCUoYwTo"; 
+window.GEMINI_API_KEY = ""; 
 
 console.log("Liberator Config Loaded");
